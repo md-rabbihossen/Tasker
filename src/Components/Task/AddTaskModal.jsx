c16816a89a0efa3bc16112856995f9f8fb1f8b67
mdrabbihossen <rabbihossenrabbi24@gmail.com>
2026-01-13 23:43:19 +0600

    Added Edit and Close a task feature

--- src/Components/Task/AddTaskModal.jsx
+++ src/Components/Task/AddTaskModal.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-export default function AddTaskModal({ onSave }) {
-  const [task, setTasks] = useState({
-    id: crypto.randomUUID(),
-    title: "",
-    description: "",
-    tags: [],
-    priority: "",
-    isFavorite: false,
-  });
+export default function AddTaskModal({ onSave, taskToUpdate, onClose }) {
+  const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null));
+  const [task, setTasks] = useState(
+    taskToUpdate || {
+      id: crypto.randomUUID(),
+      title: "",
+      description: "",
+      tags: [],
+      priority: "",
+      isFavorite: false,
+    }
+  );
   const handleChange = (evt) => {
     const name = evt.target.name;
     let value = evt.target.value;
@@ -24,11 +27,11 @@ export default function AddTaskModal({ onSave }) {
         className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/3"
         onSubmit={(e) => {
           e.preventDefault();
-          onSave(task);
+          onSave(task, isAdd);
         }}
       >
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px] ">
-          Add New Task
+          {isAdd ? " Add New Task" : "Edit Task"}
         </h2>
         {/* inputs */}
         <div className="space-y-9 text-white lg:space-y-10">
@@ -94,7 +97,14 @@ export default function AddTaskModal({ onSave }) {
           </div>
         </div>
         {/* inputs ends */}
-        <div className="mt-16 flex justify-center lg:mt-20">
+        <div className="mt-16 flex justify-between lg:mt-20">
+          <button
+            type="submit"
+            className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            onClick={onClose}
+          >
+            Close
+          </button>
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
--- src/Components/Task/TaskBoard.jsx
+++ src/Components/Task/TaskBoard.jsx
@@ -5,11 +5,30 @@ import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 import TaskSearch from "./TaskSearch";
 export default function TaskBoard() {
-  function handleAddTask(newTask) {
-    setTasks([...tasks, newTask]);
+  function handleAddTask(newTask, isAdd) {
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (newTask.id === task.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
+    }
     setShowAddModal(false);
   }
+  function handleEditTask(task) {
+    setTaskToUpdate(task);
+    setShowAddModal(true);
+  }
 
+  function handleCloseClick() {
+    setShowAddModal(false);
+    setTaskToUpdate(null);
+  }
   const defaultTask = {
     id: crypto.randomUUID(),
     title: "Learn React",
@@ -22,9 +41,16 @@ export default function TaskBoard() {
 
   const [tasks, setTasks] = useState([defaultTask]);
   const [showAddModal, setShowAddModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
   return (
     <section className="mb-20" id="tasks">
-      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
+      {showAddModal && (
+        <AddTaskModal
+          onSave={handleAddTask}
+          onClose={handleCloseClick}
+          taskToUpdate={taskToUpdate}
+        />
+      )}
       <div className="container">
         {/* Search Box */}
         <div className="p-2 flex justify-end">
@@ -33,7 +59,7 @@ export default function TaskBoard() {
         {/* Search Box Ends */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction onAddTask={() => setShowAddModal(true)} />
-          <TaskList tasks={tasks} />
+          <TaskList tasks={tasks} onEdit={handleEditTask} />
         </div>
       </div>
     </section>
